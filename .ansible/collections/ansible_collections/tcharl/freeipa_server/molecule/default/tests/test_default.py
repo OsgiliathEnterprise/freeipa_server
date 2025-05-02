6b3144078cee40ff9be90e56eab20a2663523d25
Charlie Mordant <cmordant1@gmail.com>
2025-05-02 14:12:52 +0200

    poetry 2

---
+++ .ansible/collections/ansible_collections/tcharl/freeipa_server/molecule/default/tests/test_default.py
@@ -0,0 +1,140 @@
+"""Role testing files using testinfra."""
+
+
+def test_directory_running(host):
+    command = r"""set -o pipefail && echo '123ADMin'| \
+    kinit admin > /dev/null && \
+    ipactl status | \
+    grep -c 'Directory Service: RUNNING'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_krb5kdc_running(host):
+    command = r"""set -o pipefail && echo '123ADMin'| \
+    kinit admin > /dev/null && \
+    ipactl status | \
+    grep -c 'krb5kdc Service: RUNNING'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_kadmin_running(host):
+    command = r"""set -o pipefail && echo '123ADMin'| \
+    kinit admin > /dev/null && \
+    ipactl status | \
+    grep -c 'kadmin Service: RUNNING'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_dns_running(host):
+    command = r"""set -o pipefail && echo '123ADMin'| \
+    kinit admin > /dev/null && \
+    ipactl status | \
+    grep -c 'named Service: RUNNING'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_https_running(host):
+    command = r"""set -o pipefail && echo '123ADMin'| \
+    kinit admin > /dev/null && \
+    ipactl status | \
+    grep -c 'httpd Service: RUNNING'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_custodia_running(host):
+    command = r"""set -o pipefail && echo '123ADMin'| \
+    kinit admin > /dev/null && \
+    ipactl status | \
+    grep -c 'ipa-custodia Service: RUNNING'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_pki_running(host):
+    command = r"""set -o pipefail && echo '123ADMin'| \
+    kinit admin > /dev/null && \
+    ipactl status | \
+    grep -c 'pki-tomcatd Service: RUNNING'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_otp_running(host):
+    command = r"""set -o pipefail && echo '123ADMin'| \
+    kinit admin > /dev/null && \
+    ipactl status | \
+    grep -c 'ipa-otpd Service: RUNNING'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_dnskey_running(host):
+    command = r"""set -o pipefail && echo '123ADMin'| \
+    kinit admin > /dev/null && \
+    ipactl status | \
+    grep -c 'ipa-dnskeysyncd Service: RUNNING'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_ipa_ldap_opened_in_firewall(host):
+    command = r"""set -o pipefail && \
+    firewall-cmd --list-services --zone=internal | \
+    grep -c 'freeipa-ldap'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_ipa_ldaps_opened_in_firewall(host):
+    command = r"""set -o pipefail && \
+    firewall-cmd --list-services --zone=internal | \
+    grep -c 'freeipa-ldaps'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_ipa_https_opened_in_firewall(host):
+    command = r"""set -o pipefail && \
+    firewall-cmd --list-services --zone=internal | \
+    grep -c 'https'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_ipa_dns_opened_in_firewall(host):
+    command = r"""set -o pipefail && \
+    firewall-cmd --list-services --zone=internal | \
+    grep -c 'dns'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_user_exists(host):
+    command = r"""set -o pipefail && echo '123ADMin'| \
+    kinit admin > /dev/null && \
+    ipa user-find admin | \
+    grep -c 'Last name: Administrator'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_acme_enabled(host):
+    command = r"""set -o pipefail && echo '123ADMin'| \
+    kinit admin > /dev/null && \
+    ipa-acme-manage status | \
+    grep -c 'ACME is enabled'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
+
+
+def test_https_opened_in_firewall(host):
+    command = r"""set -o pipefail && \
+    firewall-cmd --list-services --zone=internal | \
+    grep -c 'https'"""
+    cmd = host.run(command)
+    assert '1' in cmd.stdout
